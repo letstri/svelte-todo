aeb545341d10c5fcb909f4fe711767669e8c9d57
Valery Strelets <brooonsts@gmail.com>
2021-04-22 14:14:12 +0300

    minor

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,26 @@
 <script lang="ts">
-	import TodoList from './components/TodoList.svelte';
+  import TodoList from './components/TodoList.svelte';
 </script>
 
 <div class="app">
-	<TodoList />
+  <TodoList />
 </div>
 
 <style>
-	:global(*) {
-		box-sizing: border-box;
-	}
+  :global(*) {
+    box-sizing: border-box;
+  }
 
-	:global(body) {
-		margin: 0;
-		font-size: 16px;
-		font: 400 16px/1.5 sans-serif;
-		color: #333;
-	}
+  :global(body) {
+    margin: 0;
+    font-size: 16px;
+    font: 400 16px/1.5 sans-serif;
+    color: #333;
+  }
 
-	.app {
-		padding: 50px 15px;
-		max-width: 80%;
-		margin: 0 auto;
-	}
+  .app {
+    padding: 50px 15px;
+    max-width: 80%;
+    margin: 0 auto;
+  }
 </style>
